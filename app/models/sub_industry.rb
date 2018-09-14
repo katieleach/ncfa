3859e1c8f60161473fa2892f37a855afc58f2599
Spencer Dixon <spencerlloyddixon@gmail.com>
2018-09-14 13:55:18 +0100

    Finished importer for Sectors, Subindustries and Production Processes

--- app/models/sub_industry.rb
+++ app/models/sub_industry.rb
@@ -1,4 +1,4 @@
 class SubIndustry < ApplicationRecord
   belongs_to :sector
-  has_many :production_processes
+  has_many :production_processes, dependent: :destroy
 end
--- lib/modules/importer.rb
+++ lib/modules/importer.rb
@@ -5,19 +5,25 @@ module Importer
   EXPLORE_BY_ASSET              = "#{Rails.root}/lib/data/seeds/explore_by_asset.csv".freeze
   EXPLORE_BY_ECOSYSTEM_SERVICE  = "#{Rails.root}/lib/data/seeds/explore_by_ecosystem_service.csv".freeze
 
-  def self.import_sectors_and_subindustries
+  def self.import_sectors_subindustries_and_processes
     begin
-      CSV.foreach(EXPLORE_BY_ASSET, headers: true) do |row|
-        sector_name      = row[0].strip
-        subindustry_name = row[1].strip
+      CSV.foreach(EXPLORE_BY_ASSET, headers: true, encoding:'iso-8859-1:utf-8') do |row|
+        sector_name             = row[0].strip
+        sub_industry_name       = row[1].strip
+        production_process_name = row[2].strip
 
         sector = Sector.where(name: sector_name).first_or_create do |sector|
           sector.name = sector_name
         end
 
-        SubIndustry.where(name: subindustry_name).first_or_create do |subindustry|
-          subindustry.name   = subindustry_name
+        sub_industry = SubIndustry.where(name: sub_industry_name).first_or_create do |subindustry|
+          subindustry.name   = sub_industry_name
           subindustry.sector = sector
+        end
+
+        ProductionProcess.where(name: production_process_name).first_or_create do |production_process|
+          production_process.name         = production_process_name
+          production_process.sub_industry = sub_industry
         end
       end
     rescue => e
