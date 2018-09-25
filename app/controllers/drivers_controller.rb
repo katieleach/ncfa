ef347d390c575d0510da57f8a4fa2e14658f3e16
Ben Tregenna <BenT@WCMC-LT-02195s-MacBook-Pro.local>
2018-09-25 09:31:35 +0100

    Added details to drivers#show page
    not sure that materiality.justification is the correct text for the
    last column - must check w. KL

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -5,7 +5,8 @@ class DriversController < ApplicationController
 
   def show
     @driver = Driver.find(params[:id])
-    puts @driver.id
-    puts @driver.name
+    # look up production processes this driver is 'likely to influence' via assets->services->materialities
+    services = EcosystemService.find(@driver.assets.map(&:id))
+    @materialities = services.map(&:materialities).flatten(1).uniq(&:production_process)
   end
 end
