00fec9069984e036010c3b1e72bfe7b1860b26d9
Spencer Dixon <spencerlloyddixon@gmail.com>
2018-10-10 11:53:09 +0100

    Added comfy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-es_animals = EcosystemService.create(name: 'Animal-based energy')
-es_bioremediation = EcosystemService.create(name: 'Bio-remediation')
-a_species = Asset.create(name:'Species')
-d_diseases = Driver.create(name: 'Diseases')
-d_pollution = Driver.create(name: 'Pollution')
+if Rails.env.development?
+  es_animals = EcosystemService.create(name: 'Animal-based energy')
+  es_bioremediation = EcosystemService.create(name: 'Bio-remediation')
+  a_species = Asset.create(name:'Species')
+  d_diseases = Driver.create(name: 'Diseases')
+  d_pollution = Driver.create(name: 'Pollution')
 
-es_animals.assets << a_species
-a_species.drivers << d_diseases
+  es_animals.assets << a_species
+  a_species.drivers << d_diseases
 
-es_bioremediation.assets << a_species
+  es_bioremediation.assets << a_species
+end
+
+Comfy::Cms::Site.create!(identifier: 'ncfa', hostname: 'localhost')
+# Rake::Task['comfy:cms_seeds:import[ncfa, ncfa]'].invoke
