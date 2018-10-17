a83edafa3ac5f4b6ca8206347aca583caaac6c67
Spencer Dixon <spencerlloyddixon@gmail.com>
2018-10-17 10:37:28 +0100

    Moved admin credentials to encrypted secrets

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -81,8 +81,10 @@ end
 
 # Default credentials for ComfortableMexicanSofa::AccessControl::AdminAuthentication
 # YOU REALLY WANT TO CHANGE THIS BEFORE PUTTING YOUR SITE LIVE
-ComfortableMexicanSofa::AccessControl::AdminAuthentication.username = "username"
-ComfortableMexicanSofa::AccessControl::AdminAuthentication.password = "password"
+ComfortableMexicanSofa::AccessControl::AdminAuthentication.username = Rails.application.credentials[Rails.env.to_sym][:comfy][:username]
+ComfortableMexicanSofa::AccessControl::AdminAuthentication.password = Rails.application.credentials[Rails.env.to_sym][:comfy][:password]
+
+
 
 # Uncomment this module and `config.admin_auth` above to use custom admin authentication
 # module ComfyAdminAuthentication
